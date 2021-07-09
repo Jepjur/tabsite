f39b5f188f6647cadd6af9c6a21edaf2afd96d1f
Jasper <Jasper_vandijk@hotmail.com>
2021-07-09 13:36:36 +0200

    added working tabs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import Tabs from "./components/Tabs"; 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tabs Demo</h1>
+      <Tabs> 
+       <div label="Tab1"> 
+       Lorem ipsum dolor sit amet
+       </div> 
+       <div label="Tab2"> 
+       consectetur adipiscing elit
+       </div> 
+       <div label="Tab3"> 
+       ed do eiusmod
+       </div> 
+     </Tabs> 
     </div>
   );
 }
